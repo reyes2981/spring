9856c6db4ae0a196bad75d8f1b781149922ce46a
noe <noe.reyes92@yahoo.com>
2022-02-02 16:39:53 -0600

    added a GET request to user controller

--- src/main/java/com/tech/springapp/controller/UserController.java
+++ src/main/java/com/tech/springapp/controller/UserController.java
@@ -1,4 +1,24 @@
 package com.tech.springapp.controller;
 
+import com.tech.springapp.model.User;
+import com.tech.springapp.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
 public class UserController {
+
+    private final UserService userService;
+
+    @GetMapping("/users")
+    public ResponseEntity<List<User>>getUsers() {
+        return ResponseEntity.ok().body(userService.getUsers());
+    }
 }
