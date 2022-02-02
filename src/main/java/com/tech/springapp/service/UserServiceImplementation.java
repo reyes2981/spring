d4a37843cf09df8734f7175481e03596c52b6b08
noe <noe.reyes92@yahoo.com>
2022-02-02 16:19:24 -0600

    implemented methods in serservice

--- src/main/java/com/tech/springapp/service/UserServiceImplementation.java
+++ src/main/java/com/tech/springapp/service/UserServiceImplementation.java
@@ -1,4 +1,34 @@
 package com.tech.springapp.service;
 
-public class UserServiceImplementation {
+import com.tech.springapp.model.Role;
+import com.tech.springapp.model.User;
+
+import java.util.List;
+
+public class UserServiceImplementation implements UserService {
+
+    @Override
+    public User saveUser(User user) {
+        return null;
+    }
+
+    @Override
+    public Role saveRole(Role role) {
+        return null;
+    }
+
+    @Override
+    public void addRoleToUser(String username, String roleName) {
+
+    }
+
+    @Override
+    public User getUser(String username) {
+        return null;
+    }
+
+    @Override
+    public List<User> getUsers() {
+        return null;
+    }
 }
