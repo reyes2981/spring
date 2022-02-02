46c40cf900e4c8d6aeeb46770ac1c192d45c0f35
noe <noe.reyes92@yahoo.com>
2022-02-02 16:06:58 -0600

    added a id and name attribute to role class

--- src/main/java/com/tech/springapp/model/Role.java
+++ src/main/java/com/tech/springapp/model/Role.java
@@ -1,4 +1,24 @@
 package com.tech.springapp.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity // annotation defines that a class can be mapped to a table.
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "roles")
 public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+
 }
+
