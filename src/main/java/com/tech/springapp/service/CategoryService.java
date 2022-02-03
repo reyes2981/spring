93d7a1db95a1c1c28d0e27035f3ab620d7553f28
noe <noe.reyes92@yahoo.com>
2022-02-02 18:54:56 -0600

    added abstract methods to categoryservice interface

--- src/main/java/com/tech/springapp/service/CategoryService.java
+++ src/main/java/com/tech/springapp/service/CategoryService.java
@@ -1,4 +1,20 @@
 package com.tech.springapp.service;
 
+import com.tech.springapp.model.Category;
+import com.tech.springapp.model.Role;
+import com.tech.springapp.model.User;
+import com.tech.springapp.repository.CategoryRepository;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface CategoryService {
+
+    CategoryRepository setCategoryRepository(CategoryRepository categoryRepository);
+    List<Category> getCategories();
+    Optional<Category> getCategory(Long categoryId);
+    Category createCategory(Category categoryObject);
+    Category updateCategory(Long categoryId, Category categoryObject);
+    Optional<Category> deleteCategory(Long categoryId);
+
 }
