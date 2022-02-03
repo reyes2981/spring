6347916b63360bb45329b37f183aecd072ceab43
noe <noe.reyes92@yahoo.com>
2022-02-02 18:32:42 -0600

    annotated category model as a table in DB with Entity

--- src/main/java/com/tech/springapp/model/Category.java
+++ src/main/java/com/tech/springapp/model/Category.java
@@ -1,8 +1,17 @@
 package com.tech.springapp.model;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "categories")
 public class Category {
 
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column
     private String name;
 
     public Category() {
--- src/main/java/com/tech/springapp/repository/UserRepository.java
+++ src/main/java/com/tech/springapp/repository/UserRepository.java
@@ -3,6 +3,8 @@ package com.tech.springapp.repository;
 import com.tech.springapp.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+//Java Persistence API is a specification that defines an object-relational mapping (ORM) standard for storing,
+// accessing, and managing Java objects in a relational database.
 public interface UserRepository extends JpaRepository<User, Long> {
 
     User findByUsername(String username);
