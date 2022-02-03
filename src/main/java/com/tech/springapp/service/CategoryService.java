96acce6e917920dd67fe94f4895835ecfa5a645c
noe <noe.reyes92@yahoo.com>
2022-02-03 11:00:05 -0600

    created addcategory method with logix to categoryservice

--- src/main/java/com/tech/springapp/service/CategoryService.java
+++ src/main/java/com/tech/springapp/service/CategoryService.java
@@ -2,6 +2,7 @@ package com.tech.springapp.service;
 
 import com.tech.springapp.model.Category;
 import com.tech.springapp.repository.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -9,11 +10,12 @@ public class CategoryService {
 
     private final CategoryRepository categoryRepository;
 
+    @Autowired
     public CategoryService(CategoryRepository categoryRepository) {
         this.categoryRepository = categoryRepository;
     }
 
     public Category addCategory(Category category) {
-        return CategoryRepository.save(category);
+        return categoryRepository.save(category);
     }
 }
