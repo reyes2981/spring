8022f155adf74a3c46b5de2a09c7df59816a389e
noe <noe.reyes92@yahoo.com>
2022-02-03 09:46:49 -0600

    added logic to getAllCategories method

--- src/main/java/com/tech/springapp/service/CategoryServiceImplementation.java
+++ src/main/java/com/tech/springapp/service/CategoryServiceImplementation.java
@@ -1,13 +1,18 @@
 package com.tech.springapp.service;
 
 import com.tech.springapp.model.Category;
+import com.tech.springapp.repository.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
 
 public class CategoryServiceImplementation implements CategoryService {
 
+    @Autowired
+    private CategoryRepository categoryRepository;
+
     @Override
     public List<Category> getAllCategories() {
-        return null;
+        return categoryRepository.findAll();
     }
 }
