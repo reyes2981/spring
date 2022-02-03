cd4ce2093ed46be823141a4da45544d3ecb14e26
noe <noe.reyes92@yahoo.com>
2022-02-03 12:04:00 -0600

    extended JPARepo to resourceRepo

--- src/main/java/com/tech/springapp/repository/ResourceRepository.java
+++ src/main/java/com/tech/springapp/repository/ResourceRepository.java
@@ -1,7 +1,10 @@
 package com.tech.springapp.repository;
 
+import com.tech.springapp.model.Category;
+import com.tech.springapp.model.Resource;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class ResourceRepository {
+public interface ResourceRepository extends JpaRepository<Resource, Long> {
 }
