623283cd6fc595413b52d74c5b729d5c9cfc31be
noe <noe.reyes92@yahoo.com>
2022-02-02 18:39:32 -0600

    created an exception package and added infoexistexception and infonotfoundexception

--- src/main/java/com/tech/springapp/repository/CategoryRepository.java
+++ src/main/java/com/tech/springapp/repository/CategoryRepository.java
@@ -1,6 +1,7 @@
 package com.tech.springapp.repository;
 
 import com.tech.springapp.model.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
     Category findByName(String categoryName);
