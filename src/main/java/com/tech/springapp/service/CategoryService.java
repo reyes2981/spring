8e638e240197bf3b7bc826460bf531ff8193ef89
noe <noe.reyes92@yahoo.com>
2022-02-03 10:58:20 -0600

    added service annotation to categroyservice

--- src/main/java/com/tech/springapp/service/CategoryService.java
+++ src/main/java/com/tech/springapp/service/CategoryService.java
@@ -1,12 +1,19 @@
 package com.tech.springapp.service;
 
+import com.tech.springapp.model.Category;
 import com.tech.springapp.repository.CategoryRepository;
+import org.springframework.stereotype.Service;
 
+@Service
 public class CategoryService {
 
     private final CategoryRepository categoryRepository;
 
     public CategoryService(CategoryRepository categoryRepository) {
         this.categoryRepository = categoryRepository;
+    }
+
+    public Category addCategory(Category category) {
+        return CategoryRepository.save(category);
     }
 }
