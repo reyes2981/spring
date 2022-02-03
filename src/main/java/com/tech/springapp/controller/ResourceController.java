7a57f483716d441e9f06135d92657de0f281d2f5
noe <noe.reyes92@yahoo.com>
2022-02-03 14:04:13 -0600

    created ResourceDto from(Resource resource) method in resourcedto model

--- src/main/java/com/tech/springapp/controller/ResourceController.java
+++ src/main/java/com/tech/springapp/controller/ResourceController.java
@@ -1,5 +1,6 @@
 package com.tech.springapp.controller;
 
+import com.tech.springapp.model.dto.ResourceDto;
 import com.tech.springapp.service.ResourceService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
--- src/main/java/com/tech/springapp/model/dto/ResourceDto.java
+++ src/main/java/com/tech/springapp/model/dto/ResourceDto.java
@@ -1,4 +1,15 @@
 package com.tech.springapp.model.dto;
 
+import com.tech.springapp.model.Resource;
+
 public class ResourceDto {
+    private Long id;
+    private String serialNumber;
+
+    public static ResourceDto from(Resource resource) {
+        ResourceDto resourceDto = new ResourceDto();
+        resourceDto.setId(resource.getId());
+        resourceDto.setSerialNumber(resource.getSerialNumber());
+        return resourceDto;
+    }
 }
