b6cc9b2294cf10eec9f0e28e5fe38e419b7a02b8
noe <noe.reyes92@yahoo.com>
2022-02-03 13:57:28 -0600

    injected resourceservice dependency and constructor to resource controller

--- src/main/java/com/tech/springapp/controller/ResourceController.java
+++ src/main/java/com/tech/springapp/controller/ResourceController.java
@@ -1,4 +1,17 @@
 package com.tech.springapp.controller;
 
+import com.tech.springapp.service.ResourceService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/resources")
 public class ResourceController {
+
+    private final ResourceService resourceService;
+
+
+    public ResourceController(ResourceService resourceService) {
+        this.resourceService = resourceService;
+    }
 }
