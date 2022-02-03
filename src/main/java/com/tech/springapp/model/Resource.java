fec13a9cc9a365e548546bc310909102ebc68bf9
noe <noe.reyes92@yahoo.com>
2022-02-03 11:59:15 -0600

    created a resourcerepository interface

--- src/main/java/com/tech/springapp/model/Resource.java
+++ src/main/java/com/tech/springapp/model/Resource.java
@@ -1,4 +1,4 @@
 package com.tech.springapp.model;
 
-public class resource {
+public class Resource {
 }
---
+++ src/main/java/com/tech/springapp/repository/ResourceRepository.java
@@ -0,0 +1,7 @@
+package com.tech.springapp.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ResourceRepository {
+}
