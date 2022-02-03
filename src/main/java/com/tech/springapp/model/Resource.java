bbda51c83be2ccac0e2de382fcae8e2a99498537
noe <noe.reyes92@yahoo.com>
2022-02-03 12:01:45 -0600

    added serial number and id attributes to resource model

--- src/main/java/com/tech/springapp/model/Resource.java
+++ src/main/java/com/tech/springapp/model/Resource.java
@@ -1,4 +1,17 @@
 package com.tech.springapp.model;
 
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name = "resouorces")
 public class Resource {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String serialNumber;
 }
