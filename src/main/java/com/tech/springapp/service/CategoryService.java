a61ddc8f959fe0eba3b8c957abaf5e39b7ffb6b7
noe <noe.reyes92@yahoo.com>
2022-02-03 10:54:06 -0600

    converted categoryservicce from and interface to a class

--- src/main/java/com/tech/springapp/service/CategoryService.java
+++ src/main/java/com/tech/springapp/service/CategoryService.java
@@ -3,11 +3,11 @@ package com.tech.springapp.service;
 import com.tech.springapp.model.Category;
 import com.tech.springapp.model.Role;
 import com.tech.springapp.model.User;
+import com.tech.springapp.repository.CategoryRepository;
 
 import java.util.List;
 
-public interface CategoryService {
-    Category saveCategory(Category category);
-    Category getCategory(String name);
-    List<Category> getCategories();
+public class CategoryService {
+
+    private final CategoryRepository categoryRepository;
 }
