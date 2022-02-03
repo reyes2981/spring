f295f26f965618b06485b7faa8333ee8b598fb28
noe <noe.reyes92@yahoo.com>
2022-02-03 10:54:33 -0600

    added categoryservice injection with a constructor

--- src/main/java/com/tech/springapp/service/CategoryService.java
+++ src/main/java/com/tech/springapp/service/CategoryService.java
@@ -1,13 +1,12 @@
 package com.tech.springapp.service;
 
-import com.tech.springapp.model.Category;
-import com.tech.springapp.model.Role;
-import com.tech.springapp.model.User;
 import com.tech.springapp.repository.CategoryRepository;
-
-import java.util.List;
 
 public class CategoryService {
 
     private final CategoryRepository categoryRepository;
+
+    public CategoryService(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
 }
