6989c9eccc563fb8eab862ad7a77228f36569979
noe <noe.reyes92@yahoo.com>
2022-02-03 09:54:55 -0600

    added categoryservice dependency injection to category controller

--- src/main/java/com/tech/springapp/controller/CategoryController.java
+++ src/main/java/com/tech/springapp/controller/CategoryController.java
@@ -1,10 +1,23 @@
 package com.tech.springapp.controller;
 
+import com.tech.springapp.model.Category;
+import com.tech.springapp.model.User;
+import com.tech.springapp.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(path = "/api")
 public class CategoryController {
 
+    private CategoryService categoryService;
+
+    @GetMapping("/categories")
+    public ResponseEntity<List<Category>> getCategories() {
+        return ResponseEntity.ok().body(categoryService.getAllCategories());
+    }
 
 }
