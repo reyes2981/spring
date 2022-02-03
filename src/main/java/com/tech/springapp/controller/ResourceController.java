990f454a96a79aee7d100e1f3e79fe5a3dcb702e
noe <noe.reyes92@yahoo.com>
2022-02-03 14:00:19 -0600

    created addResource method to resource controller, created Dto package in model and created ResourceDto

--- src/main/java/com/tech/springapp/controller/ResourceController.java
+++ src/main/java/com/tech/springapp/controller/ResourceController.java
@@ -1,6 +1,8 @@
 package com.tech.springapp.controller;
 
 import com.tech.springapp.service.ResourceService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,8 +12,12 @@ public class ResourceController {
 
     private final ResourceService resourceService;
 
-
     public ResourceController(ResourceService resourceService) {
         this.resourceService = resourceService;
+    }
+
+    @PostMapping
+    public ResponseEntity<ResourceDto> addResource() {
+
     }
 }
